7118e524ffced3179e61dfa6a843d645729d8b51
M-Mirabella <mvesta83@gmail.com>
2020-03-08 13:11:29 +0300

    Замечания исправлены

--- HW02 - select/Queries.sql
+++ HW02 - select/Queries.sql
@@ -1,62 +1,70 @@
--- 1.
-select sti.StockItemID, sti.StockItemName
-from [Warehouse].[StockItems] sti
-where sti.StockItemName like '%urgent%' or sti.StockItemName like 'Animal%';
+-- 1. Все товары, в которых в название есть пометка urgent или название начинается с Animal
+SELECT sti.StockItemID, sti.StockItemName
+FROM [Warehouse].[StockItems] sti
+WHERE sti.StockItemName like '%urgent%' or sti.StockItemName like 'Animal%';
 
--- 2.
-select psup.SupplierID, psup.SupplierName
-from Purchasing.Suppliers psup left join
-Purchasing.PurchaseOrders pord on pord.SupplierID = psup.SupplierID
-where pord.PurchaseOrderID is null;
+-- 2.Поставщиков, у которых не было сделано ни одного заказа
+-- (потом покажем как это делать через подзапрос, сейчас сделайте через JOIN)
+SELECT psup.SupplierID, psup.SupplierName
+FROM Purchasing.Suppliers psup
+LEFT JOIN Purchasing.PurchaseOrders pord on pord.SupplierID = psup.SupplierID
+WHERE pord.PurchaseOrderID is null;
 
 -- но привычнее и быстрее вот так:
-select psup.SupplierID, psup.SupplierName
-from Purchasing.Suppliers psup
-where not exists (select *
-from Purchasing.PurchaseOrders pord where pord.SupplierID = psup.SupplierID);
+SELECT psup.SupplierID, psup.SupplierName
+FROM Purchasing.Suppliers psup
+WHERE not exists (SELECT *
+FROM Purchasing.PurchaseOrders pord
+WHERE pord.SupplierID = psup.SupplierID);
 
---3 
-select datename(month, sord.OrderDate) NameMonth, CEILING(month(sord.OrderDate)/3.00) as QuarterNumber,
+--3 Продажи с названием месяца, в котором была продажа, номером квартала, к которому относится продажа,
+/*включите также к какой трети года относится дата - каждая треть по 4 месяца, дата забора заказа должна быть задана,
+с ценой товара более 100$ либо количество единиц товара более 20.
+Добавьте вариант этого запроса с постраничной выборкой пропустив первую 1000 и отобразив следующие 100 записей.
+Соритровка должна быть по номеру квартала, трети года, дате продажи.*/
+
+SELECT datename(month, sord.OrderDate) NameMonth, CEILING(month(sord.OrderDate)/3.00) as QuarterNumber,
 CEILING(month(sord.OrderDate)/4.00) as ThirdNumber, sord.OrderDate, wsti.StockItemName, sorlin.Quantity, sorlin.UnitPrice
-from Sales.Orders sord inner join
-Sales.OrderLines sorlin on sorlin.OrderID = sord.OrderID inner join
-Warehouse.StockItems wsti on wsti.StockItemID = sorlin.StockItemID
-where sord.ExpectedDeliveryDate between cast('2014-04-02' as date) and cast('2014-04-30' as date)
+FROM Sales.Orders sord
+INNER JOIN Sales.OrderLines sorlin on sorlin.OrderID = sord.OrderID
+INNER JOIN Warehouse.StockItems wsti on wsti.StockItemID = sorlin.StockItemID
+WHERE sord.ExpectedDeliveryDate between cast('2014-04-02' as date) and cast('2014-04-30' as date)
 and (sorlin.UnitPrice > 100 or sorlin.Quantity > 20);
 
-select datename(month, sord.OrderDate) NameMonth, CEILING(month(sord.OrderDate)/3.00) as QuarterNumber,
+SELECT datename(month, sord.OrderDate) NameMonth, CEILING(month(sord.OrderDate)/3.00) as QuarterNumber,
 CEILING(month(sord.OrderDate)/4.00) as ThirdNumber, sord.OrderDate, wsti.StockItemName, sorlin.Quantity, sorlin.UnitPrice
-from Sales.Orders sord inner join
-Sales.OrderLines sorlin on sorlin.OrderID = sord.OrderID inner join
-Warehouse.StockItems wsti on wsti.StockItemID = sorlin.StockItemID
-where sord.ExpectedDeliveryDate between cast('2014-10-15' as date) and cast('2014-10-30' as date)
+FROM Sales.Orders sord
+INNER JOIN Sales.OrderLines sorlin on sorlin.OrderID = sord.OrderID
+INNER JOIN Warehouse.StockItems wsti on wsti.StockItemID = sorlin.StockItemID
+WHERE sord.ExpectedDeliveryDate between cast('2014-10-15' as date) and cast('2014-10-30' as date)
 and (sorlin.UnitPrice > 100 or sorlin.Quantity > 20)
 order by QuarterNumber, ThirdNumber, sord.OrderDate
 offset 1000 rows fetch next 100 rows only;
 
---4
-select psup.SupplierName, adelm.DeliveryMethodName, pord.ExpectedDeliveryDate, apeop.FullName
-from Purchasing.PurchaseOrders pord inner join
-Application.DeliveryMethods adelm on adelm.DeliveryMethodID = pord.DeliveryMethodID inner join
-Purchasing.Suppliers psup on psup.SupplierID = pord.SupplierID inner join
-Application.People apeop on apeop.PersonID = pord.ContactPersonID
-where pord.ExpectedDeliveryDate between cast('2014-01-01' as date) and cast('2014-12-31' as date) and
+--4 Заказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post,
+--добавьте название поставщика, имя контактного лица принимавшего заказ
+SELECT psup.SupplierName, adelm.DeliveryMethodName, pord.ExpectedDeliveryDate, apeop.FullName
+FROM Purchasing.PurchaseOrders pord
+INNER JOIN Application.DeliveryMethods adelm on adelm.DeliveryMethodID = pord.DeliveryMethodID
+INNER JOIN Purchasing.Suppliers psup on psup.SupplierID = pord.SupplierID
+INNER JOIN Application.People apeop on apeop.PersonID = pord.ContactPersonID
+WHERE pord.ExpectedDeliveryDate between cast('2014-01-01' as date) and cast('2014-12-31' as date) and
 adelm.DeliveryMethodName in ('Road Freight', 'Post');
 
---5
-select top 10 scust.CustomerName, sord.OrderDate, apeop.FullName
-from Sales.Orders sord inner join
-Sales.Customers scust on scust.CustomerID = sord.CustomerID inner join
-Application.People apeop on apeop.PersonID = sord.SalespersonPersonID
+--5 10 последних по дате продаж с именем клиента и именем сотрудника, который оформил заказ.
+SELECT top 10 scust.CustomerName, sord.OrderDate, apeop.FullName
+FROM Sales.Orders sord
+INNER JOIN Sales.Customers scust on scust.CustomerID = sord.CustomerID
+INNER JOIN Application.People apeop on apeop.PersonID = sord.SalespersonPersonID
 order by sord.OrderDate desc
 
---6
-select distinct scust.CustomerID, scust.CustomerName, scust.PhoneNumber
-from Sales.OrderLines sorl inner join
-Sales.Orders sord on sord.OrderID = sorl.OrderID inner join
-Sales.Customers scust on scust.CustomerID = sord.CustomerID inner join
-Warehouse.StockItems wsti on wsti.StockItemID = sorl.StockItemID --inner join
-where wsti.StockItemName = 'Chocolate frogs 250g'
+--6 Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g
+SELECT distinct scust.CustomerID, scust.CustomerName, scust.PhoneNumber
+FROM Sales.OrderLines sorl
+INNER JOIN Sales.Orders sord on sord.OrderID = sorl.OrderID
+INNER JOIN Sales.Customers scust on scust.CustomerID = sord.CustomerID
+INNER JOIN Warehouse.StockItems wsti on wsti.StockItemID = sorl.StockItemID
+WHERE wsti.StockItemName = 'Chocolate frogs 250g'
 
 
 
