ae97e7cb4e578f309d9e89ef3c6ed94ec9215a44
M-Mirabella <mvesta83@gmail.com>
2020-04-13 21:32:17 +0300

    Исправление ДЗ

--- HW07 - window/SQLQuery.sql
+++ HW07 - window/SQLQuery.sql
@@ -29,16 +29,23 @@ SELECT DISTINCT inv.InvoiceDate, InvSums.InvSum
 FROM Sales.Invoices inv
 CROSS APPLY (SELECT SUM(invl.Quantity*invl.UnitPrice) as InvSum
 			 FROM Sales.InvoiceLines invl
-			 INNER JOIN Sales.Invoices inv_1 on invl.InvoiceID = inv_1.InvoiceID
+			 INNER JOIN Sales.Invoices inv_1
+				on invl.InvoiceID = inv_1.InvoiceID
 			 WHERE DATEFROMPARTS(YEAR(inv_1.InvoiceDate), MONTH(inv_1.InvoiceDate), 1 ) <= DATEFROMPARTS(YEAR(inv.InvoiceDate), MONTH(inv.InvoiceDate), 1 )
 			 ) as InvSums
 
-select inv.InvoiceID, Customers.CustomerName, inv.InvoiceDate, invl.Quantity*invl.UnitPrice as SaleAmount,
-#InvSums.InvSum As CumulativeAmount
+select  inv.InvoiceID,
+		Customers.CustomerName,
+		inv.InvoiceDate,
+		invl.Quantity*invl.UnitPrice as SaleAmount,
+		#InvSums.InvSum As CumulativeAmount
 FROM Sales.Invoices inv
-INNER JOIN Sales.Customers on Customers.CustomerID = inv.CustomerID
-INNER JOIN Sales.InvoiceLines invl on invl.InvoiceID = inv.InvoiceID
-INNER JOIN #InvSums on #InvSums.InvoiceDate = inv.InvoiceDate
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = inv.CustomerID
+	INNER JOIN Sales.InvoiceLines invl
+		on invl.InvoiceID = inv.InvoiceID
+	INNER JOIN #InvSums
+		on #InvSums.InvoiceDate = inv.InvoiceDate
 ORDER BY inv.InvoiceDate
 
 -- 2. Вариант с табличной переменной
@@ -52,16 +59,23 @@ SELECT DISTINCT inv.InvoiceDate, InvSums.InvSum
 FROM Sales.Invoices inv
 CROSS APPLY (SELECT SUM(invl.Quantity*invl.UnitPrice) as InvSum
 			 FROM Sales.InvoiceLines invl
-			 INNER JOIN Sales.Invoices inv_1 on invl.InvoiceID = inv_1.InvoiceID
+			INNER JOIN Sales.Invoices inv_1
+				on invl.InvoiceID = inv_1.InvoiceID
 			 WHERE DATEFROMPARTS(YEAR(inv_1.InvoiceDate), MONTH(inv_1.InvoiceDate), 1 ) <= DATEFROMPARTS(YEAR(inv.InvoiceDate), MONTH(inv.InvoiceDate), 1 )
 			 ) as InvSums
 
-select inv.InvoiceID, Customers.CustomerName, inv.InvoiceDate, invl.Quantity*invl.UnitPrice as SaleAmount,
-InvSums.InvSum As CumulativeAmount
+select  inv.InvoiceID,
+		Customers.CustomerName,
+		inv.InvoiceDate,
+		invl.Quantity*invl.UnitPrice as SaleAmount,
+		InvSums.InvSum As CumulativeAmount
 FROM Sales.Invoices inv
-INNER JOIN Sales.Customers on Customers.CustomerID = inv.CustomerID
-INNER JOIN Sales.InvoiceLines invl on invl.InvoiceID = inv.InvoiceID
-INNER JOIN @InvSums as InvSums on InvSums.InvoiceDate = inv.InvoiceDate
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = inv.CustomerID
+	INNER JOIN Sales.InvoiceLines invl
+		on invl.InvoiceID = inv.InvoiceID
+	INNER JOIN @InvSums as InvSums
+		on InvSums.InvoiceDate = inv.InvoiceDate
 ORDER BY inv.InvoiceDate
 
 -- По поводу планов:
@@ -73,11 +87,16 @@ ORDER BY inv.InvoiceDate
 --2. Если вы брали предложенный выше запрос, то сделайте расчет суммы нарастающим итогом с помощью оконной функции.
 --SET STATISTICS TIME ON
 
-select inv.InvoiceID, Customers.CustomerName, inv.InvoiceDate, invl.Quantity*invl.UnitPrice as SaleAmount,
-SUM(invl.Quantity*invl.UnitPrice) OVER(ORDER BY DATEFROMPARTS(YEAR(inv.InvoiceDate), MONTH(inv.InvoiceDate), 1 )) As CumulativeAmount
+select  inv.InvoiceID,
+		Customers.CustomerName,
+		inv.InvoiceDate,
+		invl.Quantity*invl.UnitPrice as SaleAmount,
+		SUM(invl.Quantity*invl.UnitPrice) OVER(ORDER BY DATEFROMPARTS(YEAR(inv.InvoiceDate), MONTH(inv.InvoiceDate), 1 )) As CumulativeAmount
 FROM Sales.Invoices inv
-INNER JOIN Sales.Customers on Customers.CustomerID = inv.CustomerID
-INNER JOIN Sales.InvoiceLines invl on invl.InvoiceID = inv.InvoiceID
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = inv.CustomerID
+	INNER JOIN Sales.InvoiceLines invl
+		on invl.InvoiceID = inv.InvoiceID
 ORDER BY inv.InvoiceDate
 
 --Сравните 2 варианта запроса - через windows function и без них. Написать какой быстрее выполняется, сравнить по set statistics time on;
@@ -87,18 +106,23 @@ ORDER BY inv.InvoiceDate
 
 --2. Вывести список 2х самых популярных продуктов (по кол-ву проданных) в каждом месяце за 2016й год (по 2 самых популярных продукта в каждом месяце)
 
-with cte_quantity as (SELECT sti.StockItemID, sti.StockItemName, MONTH(inv.InvoiceDate) as Month_InvDate,
-SUM(invl.Quantity) as Sum_Quantity
+with cte_quantity as (
+SELECT  sti.StockItemID,
+		sti.StockItemName,
+		MONTH(inv.InvoiceDate) as Month_InvDate,
+		SUM(invl.Quantity) as Sum_Quantity
 FROM Sales.InvoiceLines invl 
-INNER JOIN Sales.Invoices inv on inv.InvoiceID = invl.InvoiceID
-INNER JOIN Warehouse.StockItems sti on sti.StockItemID = invl.StockItemID
+	INNER JOIN Sales.Invoices inv
+		on inv.InvoiceID = invl.InvoiceID
+	INNER JOIN Warehouse.StockItems sti
+		on sti.StockItemID = invl.StockItemID
 GROUP BY sti.StockItemID, sti.StockItemName, MONTH(inv.InvoiceDate)
 )
 
 SELECT *
 FROM (SELECT StockItemID, StockItemName, Month_InvDate, Sum_Quantity, 
-ROW_NUMBER() OVER (PARTITION BY Month_InvDate ORDER BY Sum_Quantity DESC) as Quantity_Rank
-FROM cte_quantity) as tbl
+		ROW_NUMBER() OVER (PARTITION BY Month_InvDate ORDER BY Sum_Quantity DESC) as Quantity_Rank
+	  FROM cte_quantity) as tbl
 WHERE Quantity_Rank <= 2
 
 --3. Функции одним запросом
@@ -112,13 +136,17 @@ WHERE Quantity_Rank <= 2
 --сформируйте 30 групп товаров по полю вес товара на 1 шт
 --Для этой задачи НЕ нужно писать аналог без аналитических функций
 
-SELECT sti.StockItemID, sti.StockItemName, sti.Brand, sti.UnitPrice, LEFT(sti.StockItemName, 1),
-ROW_NUMBER() OVER(PARTITION BY LEFT(sti.StockItemName, 1) ORDER BY sti.StockItemName) as rNumber,
-COUNT(*) OVER() as TotalCount, COUNT(*) OVER(PARTITION BY LEFT(sti.StockItemName, 1)) as CountByLetter,
-LEAD(sti.StockItemID) OVER(ORDER BY sti.StockItemName) as NextID,
-LAG(sti.StockItemID) OVER(ORDER BY sti.StockItemName) as PrevID,
-LAG(sti.StockItemName, 2, 'No items') OVER(ORDER BY sti.StockItemName) as PrevStItName,
-NTILE(30) OVER(ORDER BY sti.TypicalWeightPerUnit) as Groups
+SELECT  sti.StockItemID,
+		sti.StockItemName,
+		sti.Brand,
+		sti.UnitPrice,
+		LEFT(sti.StockItemName, 1),
+		ROW_NUMBER() OVER(PARTITION BY LEFT(sti.StockItemName, 1) ORDER BY sti.StockItemName) as rNumber,
+		COUNT(*) OVER() as TotalCount, COUNT(*) OVER(PARTITION BY LEFT(sti.StockItemName, 1)) as CountByLetter,
+		LEAD(sti.StockItemID) OVER(ORDER BY sti.StockItemName) as NextID,
+		LAG(sti.StockItemID) OVER(ORDER BY sti.StockItemName) as PrevID,
+		LAG(sti.StockItemName, 2, 'No items') OVER(ORDER BY sti.StockItemName) as PrevStItName,
+		NTILE(30) OVER(ORDER BY sti.TypicalWeightPerUnit) as Groups
 FROM Warehouse.StockItems sti
 ORDER BY sti.StockItemName
 
@@ -127,36 +155,69 @@ ORDER BY sti.StockItemName
 
 -- Получилось два варианта. Как считаете какой вариант лучше? По плану вроде как первый.
 -- Но по статистике времени у меня получается что второй
-SELECT *
-FROM (SELECT People.PersonID, People.FullName, Customers.CustomerID, Customers.CustomerName, inv.InvoiceDate,
-(SELECT SUM(Invl.Quantity*Invl.UnitPrice) 
-FROM Sales.InvoiceLines Invl WHERE Invl.InvoiceID = inv.InvoiceID) as SaleAmount,
-ROW_NUMBER() OVER(PARTITION BY People.PersonID ORDER BY inv.InvoiceID DESC) AS rNumber
+-- Вариант 1
+WITH cte_tbl as (
+SELECT  People.PersonID,
+		People.FullName,
+		Customers.CustomerID,
+		Customers.CustomerName,
+		inv.InvoiceDate,
+		(SELECT SUM(Invl.Quantity*Invl.UnitPrice) 
+			FROM Sales.InvoiceLines Invl
+		 WHERE Invl.InvoiceID = inv.InvoiceID) as SaleAmount,
+		ROW_NUMBER() OVER(PARTITION BY People.PersonID ORDER BY inv.InvoiceID DESC) AS rNumber
 FROM Sales.Invoices inv
-INNER JOIN Application.People on People.PersonID = inv.SalespersonPersonID
-INNER JOIN Sales.Customers on Customers.CustomerID = inv.CustomerID) as tbl
+	INNER JOIN Application.People
+		on People.PersonID = inv.SalespersonPersonID
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = inv.CustomerID)
+
+SELECT *
+FROM cte_tbl
 WHERE rNumber = 1
 ORDER BY PersonID
 
-SELECT People.PersonID, People.FullName, Customers.CustomerID, Customers.CustomerName, Invoices.InvoiceDate,
-(SELECT SUM(Invl.Quantity*Invl.UnitPrice) 
-FROM Sales.InvoiceLines Invl WHERE Invl.InvoiceID = Invoices.InvoiceID) as SaleAmount
-FROM (SELECT DISTINCT LAST_VALUE(inv.InvoiceId) OVER(PARTITION BY inv.SalespersonPersonID ORDER BY inv.InvoiceId ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) as MaxID
-FROM Sales.Invoices inv) as inv
-INNER JOIN Sales.Invoices on Invoices.InvoiceID = inv.MaxID
-INNER JOIN Application.People on People.PersonID = Invoices.SalespersonPersonID
-INNER JOIN Sales.Customers on Customers.CustomerID = Invoices.CustomerID
+-- Вариант 2
+WITH inv as (
+SELECT DISTINCT LAST_VALUE(InvoiceId) OVER(PARTITION BY SalespersonPersonID
+			                               ORDER BY InvoiceId
+										   ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) as MaxID
+FROM Sales.Invoices)
+
+SELECT  People.PersonID,
+		People.FullName,
+		Customers.CustomerID,
+		Customers.CustomerName,
+		Invoices.InvoiceDate,
+		(SELECT SUM(Invl.Quantity*Invl.UnitPrice) 
+		 FROM Sales.InvoiceLines Invl
+		 WHERE Invl.InvoiceID = Invoices.InvoiceID) as SaleAmount
+FROM inv
+	INNER JOIN Sales.Invoices
+		on Invoices.InvoiceID = inv.MaxID
+	INNER JOIN Application.People
+		on People.PersonID = Invoices.SalespersonPersonID
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = Invoices.CustomerID
 ORDER BY People.PersonID
 
 --5. Выберите по каждому клиенту 2 самых дорогих товара, которые он покупал
 --В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки
-SELECT *
-FROM (SELECT DISTINCT Customers.CustomerID, Customers.CustomerName, invl.StockItemID, 
-invl.UnitPrice, inv.InvoiceDate,
-DENSE_RANK() OVER(PARTITION BY Customers.CustomerID ORDER BY invl.UnitPrice DESC) as rRank
+WITH cte_tbl as (
+SELECT DISTINCT Customers.CustomerID,
+				Customers.CustomerName,
+				invl.StockItemID, 
+				invl.UnitPrice,
+				inv.InvoiceDate,
+				DENSE_RANK() OVER(PARTITION BY Customers.CustomerID ORDER BY invl.UnitPrice DESC) as rRank
 FROM Sales.Invoices inv
-INNER JOIN Sales.InvoiceLines invl on inv.InvoiceID = invl.InvoiceID
-INNER JOIN Sales.Customers on Customers.CustomerID = inv.CustomerID) as tbl
+	INNER JOIN Sales.InvoiceLines invl
+		on inv.InvoiceID = invl.InvoiceID
+	INNER JOIN Sales.Customers
+		on Customers.CustomerID = inv.CustomerID)
+
+SELECT *
+FROM cte_tbl
 WHERE rRank <= 2
 ORDER BY CustomerID, StockItemID, InvoiceDate
 
